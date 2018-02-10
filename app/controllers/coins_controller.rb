ceffd49787c5e8fb412d99baf69a6bba39deeb89
Nick Krupa <nick@workingdev.com>
2018-02-10 14:00:39 -0600

    simple kaminari installation

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -1,7 +1,7 @@
 class CoinsController < ApplicationController
 
   def index
-    @coins ||= Coin.positive_fa_score.order("tri_fa_score desc, cryptocompare_rank asc")
+    @coins = Coin.positive_fa_score.order("tri_fa_score desc, cryptocompare_rank asc").page(params[:page])
   end
 
   def watchlist
